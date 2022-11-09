2cc5fdd44b955471ab7c009a917fa1a4038fbbd2
tracer-compiler2022 <compilers@software.nju.edu.cb>
2022-11-09 20:23:24 +0800

    > make 201830176 root Linux zfm-virtual-machine-20-04 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:23:24 up  1:43,  1 user,  load average: 0.29, 0.21, 0.14 4fd2ce2aa5a4ce116850babf859e1bfe75a93

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,15 @@ import java.util.Scanner;
 public class Main
 {    
     public static void main(String[] args) throws IOException {
-        public static void main(String[] args) throws IOException {
-            if (args.length < 1) {
-                System.err.println("input path is required");
-            }
-            String source = args[0];
-            CharStream input = CharStreams.fromFileName(source);
-            SysYLexer sysYLexer = new SysYLexer(input);
+        //     public static void main(String[] args) throws IOException {
+//            if (args.length < 1) {
+//                System.err.println("input path is required");
+//            }
+//            String source = args[0];
+//            CharStream input = CharStreams.fromFileName(source);
+//            SysYLexer sysYLexer = new SysYLexer(input);
+  //  }
         }
 
-    }
 }
+

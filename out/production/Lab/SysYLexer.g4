cf60da495fed66f647f21b7413ab7ab482c90bfe
tracer-compiler2022 <compilers@software.nju.edu.cb>
2022-11-09 20:47:53 +0800

    > make 201830176 root Linux zfm-virtual-machine-20-04 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:47:53 up  2:07,  1 user,  load average: 0.37, 0.42, 0.34 16e299d9a21432c9fa1d37955a2f1273d7fa2580

---
+++ out/production/Lab/SysYLexer.g4
@@ -0,0 +1,24 @@
+grammar SysYLexer;
+
+prog : stat* EOF;
+
+stat: expr ';'
+      | ID '=' expr ';'
+      | 'if' expr ';'
+      ;
+
+expr: expr ('*'|'/') expr
+     | expr ('+'|'-') expr
+     | ID
+     | INT
+     ;
+
+ID : (LETTER | '_')(LETTER |DIGIT | '_')*;
+
+INT : '0' | ([1-9] [0-9]*) ;
+
+WS : [ \t\r\n]+ -> skip ;
+
+fragment LETTER: [a-zA-Z] ;
+fragment DIGIT: [0-9] ;
+
--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,24 @@
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 
+import javax.xml.bind.SchemaOutputResolver;
 import java.io.*;
 import java.util.Scanner;
 
 public class Main
 {    
     public static void main(String[] args) throws IOException {
-        //     public static void main(String[] args) throws IOException {
-//            if (args.length < 1) {
-//                System.err.println("input path is required");
-//            }
-//            String source = args[0];
-//            CharStream input = CharStreams.fromFileName(source);
-//            SysYLexer sysYLexer = new SysYLexer(input);
-  //  }
-        }
+
+            if (args.length < 1) {
+                System.err.println("input path is required");
+            }
+          //  String source = args[0];
+           // CharStream input = CharStreams.fromFileName(source);
+         //   SysYLexer sysYLexer = new SysYLexer(input);
+        System.out.println(2);
+    }
+
+
 
 }
 

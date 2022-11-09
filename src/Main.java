b9a6844d069d45a5c7908a9755abc0a82d02548d
tracer-compiler2022 <compilers@software.nju.edu.cb>
2022-11-09 14:35:24 +0800

    > make 201830176 root Linux zfm-virtual-machine-20-04 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:35:24 up 39 min,  1 user,  load average: 0.48, 0.35, 0.23 b03611858b6566a742eb471849312e3b3f97d409

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 public class Main
 {    
     public static void main(String[] args) throws IOException {
-        String cmd="";
-        Scanner scanner=new Scanner(System.in);
-        cmd=scanner.nextLine();
-        String[]opts=cmd.split(" ");
-        if(opts[0].equals("cat")){
-            File file=new File("src/1");
+      //  String cmd="";
+      //  Scanner scanner=new Scanner(System.in);
+      //  cmd=scanner.nextLine();
+      //  String[]opts=cmd.split(" ");
+        if(args[0].equals("cat")){
+            File file=new File(args[1]);
             FileReader fileReader=new FileReader(file);
             BufferedReader br=new BufferedReader(fileReader);
             String line="";

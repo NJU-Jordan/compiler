165779d06f1dff006e96a65458a456d0113d9de4
tracer-compiler2022 <compilers@software.nju.edu.cb>
2022-11-09 21:57:21 +0800

    > make 201830176 root Linux zfm-virtual-machine-20-04 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:57:21 up  3:17,  1 user,  load average: 0.07, 0.19, 0.24 b9099457c003dc7726527480eb8a111d30d6aa5a

--- out/production/Lab/SysYLexerBaseVisitor.java
+++ out/production/Lab/SysYLexerBaseVisitor.java
@@ -1,4 +1,4 @@
-// Generated from ./out/production/Lab/SysYLexer.g4 by ANTLR 4.10.1
+// Generated from ./out/production/Lab/SysYLexer.g4 by ANTLR 4.9.1
 import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
 
 /**

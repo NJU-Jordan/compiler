a4b24add360562f8a01e20af0c6213f6b7586834
tracer-compiler2022 <compilers@software.nju.edu.cb>
2022-11-10 17:29:02 +0800

    > make 201830176 root Linux zfm-virtual-machine-20-04 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:29:02 up  2:07,  1 user,  load average: 0.65, 0.40, 0.29 2adb864fa09eef6a79059e113e405157444062ae

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,24 @@ import org.antlr.v4.runtime.Token;
 
 
 import java.io.*;
+import java.math.BigInteger;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main
 {
-    public static void main(String[] args) throws IOException {
+    public static String toDEC(String INTEGR_CONST){
 
+        String result=INTEGR_CONST;
+        if(INTEGR_CONST.startsWith("0")){
+
+        }else if(INTEGR_CONST.startsWith("0x")){
+            result =String.valueOf(Integer.parseInt(INTEGR_CONST,16));
+        }
+        return  result;
+    }
+    public static void main(String[] args) throws IOException {
+        System.out.println(toDEC("0xf"));
         if (args.length < 1) {
             System.err.println("input path is required");
         }

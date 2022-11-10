ccf71e62fa4bbb004df077d73a4917ae017aa789
tracer-compiler2022 <compilers@software.nju.edu.cb>
2022-11-10 10:52:41 +0800

    > make 201830176 root Linux zfm-virtual-machine-20-04 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:52:41 up 22 min,  1 user,  load average: 0.20, 0.25, 0.26 fccf03b83ed4a6274f9e2de95794ab18301d423c

---
+++ src/ErrorListener.java
@@ -0,0 +1,5 @@
+import org.antlr.v4.runtime.BaseErrorListener;
+
+public class ErrorListener extends BaseErrorListener {
+
+}
--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,20 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Main
-{    
+{
     public static void main(String[] args) throws IOException {
-
+        if (args.length < 1) {
+            System.err.println("input path is required");
+        }
+        String source = args[0];
+        CharStream input = CharStreams.fromFileName(source);
+        SysYLexer sysYLexer = new SysYLexer(input);
+        sysYLexer.removeErrorListeners();
+     //   ErrorListener myErrorListener=new ErrorListener();
+     //   sysYLexer.addErrorListener(myErrorListener);
+        System.out.println(sysYLexer.getAllTokens());
 
     }
-
 
 
 }

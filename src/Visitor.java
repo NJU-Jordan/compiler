b81fbea8e337d940c4d986fb7a0f7a2caec0d434
tracer-compiler2022 <compilers@software.nju.edu.cb>
2022-11-23 13:08:05 +0800

    > make 201830176 zhangfengming Linux zfm-virtual-machine-20-04 5.15.0-53-generic #59~20.04.1-Ubuntu SMP Thu Oct 20 15:10:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:08:05 up  4:48,  1 user,  load average: 0.59, 0.50, 0.42 592fea17acfb4b266655454a1838176aee00a9ae

--- src/Visitor.java
+++ src/Visitor.java
@@ -11,6 +11,13 @@ public class Visitor extends  SysYParserBaseVisitor{
 
     private Vocabulary vocabulary;
     private int cur_depth;  //记录当前访问树的深度
+    private String[] rule_colors= new String[] {
+        null, "CONST[orange]", "INT[orange]", "VOID[orange]", "IF[orange]", "ELSE[orange]", "WHILE[orange]", "BREAK[orange]", "CONTINUE[orange]",
+                "RETURN[orange]", "PLUS[blue]", "MINUS[blue]", "MUL[blue]", "DIV[blue]", "MOD[blue]", "ASSIGN[blue]", "EQ[blue]", "NEQ[blue]",
+                "LT[blue]", "GT[blue]", "LE[blue]", "GE[blue]", "NOT[blue]", "AND[blue]", "OR[blue]", "", "", "",
+                "", "", "", "", "", "IDENT[red]", "INTEGR_CONST[green]",
+                "", "", ""
+    };
 
     public Visitor(SysYLexer sysYLexer, SysYParser sysYParser){
         this.sysYLexer=sysYLexer;
@@ -35,10 +42,10 @@ public class Visitor extends  SysYParserBaseVisitor{
 
     @Override
     public Object visitTerminal(TerminalNode node) {
-       String text=node.getText();
+       //String text=node.getText();
        int type=node.getSymbol().getType();
        cur_depth++;
-        System.err.println(indent_of_depth(cur_depth)+text+" "+vocabulary.getSymbolicName(type));
+        System.err.println(indent_of_depth(cur_depth)+rule_colors[type]);
       //  System.err.println(index);
         cur_depth--;
         return super.visitTerminal(node);

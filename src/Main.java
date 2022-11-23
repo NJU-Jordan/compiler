f614d8ac2840285818b5c69c50a50708bd3941a7
tracer-compiler2022 <compilers@software.nju.edu.cb>
2022-11-23 18:54:42 +0800

    > make 201830176 zhangfengming Linux zfm-virtual-machine-20-04 5.15.0-53-generic #59~20.04.1-Ubuntu SMP Thu Oct 20 15:10:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:54:42 up  9:56,  1 user,  load average: 0.72, 0.29, 0.55 a2335ab0622cbe4276682330ba33488a894a2902

--- src/Main.java
+++ src/Main.java
@@ -26,8 +26,7 @@ public class Main
         return  result;
     }
     public static void main(String[] args) throws IOException {
-        //       System.out.println(toDEC("0"));
- //命令行参数部分
+         //      System.out.println(toDEC("0"));
         if (args.length < 1) {
             System.err.println("input path is required");
         }

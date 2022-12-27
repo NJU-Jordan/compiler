dd33eed62ad26faa9386f1a97a5a05471c5d11ee
tracer-compiler2022 <compilers@software.nju.edu.cb>
2022-12-27 10:34:04 +0800

    > make 201830176 zhangfengming Linux virtual-machine 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:34:04 up 15:56,  1 user,  load average: 0.63, 0.52, 0.53 70ee592168c5e0b2bcbd84b86900211a8e449bb1

--- src/Main.java
+++ src/Main.java
@@ -27,7 +27,7 @@ public class Main
         return  result;
     }
     public static void main(String[] args) throws IOException {
-               System.out.println(toDEC("0"));
+
         if (args.length < 4) {
             System.err.println("input path is required");
         }

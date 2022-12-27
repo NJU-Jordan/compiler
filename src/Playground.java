9231979a96d1e8626c568d67230e73afddd2402c
tracer-compiler2022 <compilers@software.nju.edu.cb>
2022-12-27 08:19:06 +0800

    > make 201830176 zhangfengming Linux virtual-machine 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:19:06 up 13:41,  1 user,  load average: 0.26, 0.24, 0.19 429c667d7b0c0ad56ef57963be3f7fd92ea0d324

--- src/Playground.java
+++ src/Playground.java
@@ -6,38 +6,14 @@ import java.io.IOException;
 import java.util.List;
 
 public class Playground {
-    public static Integer toDEC(String INTEGR_CONST){
 
-        Integer result;
-        if(INTEGR_CONST.startsWith("0x")){
-            result =Integer.parseInt(INTEGR_CONST.substring(2),16);
-        }else if(INTEGR_CONST.startsWith("0")&&INTEGR_CONST.length()>=2){
-            result = Integer.parseInt(INTEGR_CONST.substring(1),8);
-        }
-        else{
-            result=Integer.parseInt(INTEGR_CONST);
-        }
-        return  result;
+    public static int A(){
+        return  2;
     }
     public static void main(String[] args) throws IOException {
-        System.out.println(toDEC("0"));
-
-
-        CharStream input = CharStreams.fromFileName("src/input.txt");
-        SysYLexer sysYLexer = new SysYLexer(input);
-        sysYLexer.removeErrorListeners();
-        //   ErrorListener myErrorListener=new ErrorListener();
-        //   sysYLexer.addErrorListener(myErrorListener);
-        List<?extends Token> tokens =sysYLexer.getAllTokens();
-        System.err.println(tokens);
-        for(Token token:tokens){
-            if(sysYLexer.getRuleNames()[token.getType()-1].equals("INTEGR_CONST")){
-                int  num=toDEC(token.getText());
-                System.err.println(sysYLexer.getRuleNames()[token.getType()-1]+" "+num+" at Line "+token.getLine()+".");
-            }
-            else System.err.println(sysYLexer.getRuleNames()[token.getType()-1]+" "+token.getText()+" at Line "+token.getLine()+".");
-        }
-
+       int A;
+       A= A();
+        System.out.println(A);
 
     }
 }
